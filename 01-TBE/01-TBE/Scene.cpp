8adbb40ba15eaf528b06935b454f51f349819a0b
pere.carrillo.barrera <pere.carrillo.barrera@stark.fib.upc.edu>
2023-02-28 13:44:30 +0100

    initial commit

---
+++ 01-TBE/01-TBE/Scene.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <cmath>
+#include <glm/gtc/matrix_transform.hpp>
+#include "Scene.h"
+
+
+Scene::Scene()
+{
+	map = NULL;
+}
+
+Scene::~Scene()
+{
+	if(map != NULL)
+		delete map;
+}
+
+
+void Scene::init()
+{
+	initShaders();
+	map = TileMap::createTileMap("levels/level01.txt", glm::vec2(32, 16), texProgram);
+	projection = glm::ortho(0.f, float(CAMERA_WIDTH - 1), float(CAMERA_HEIGHT - 1), 0.f);
+	currentTime = 0.0f;
+}
+
+void Scene::update(int deltaTime)
+{
+	currentTime += deltaTime;
+}
+
+void Scene::render()
+{
+	glm::mat4 modelview;
+
+	texProgram.use();
+	texProgram.setUniformMatrix4f("projection", projection);
+	texProgram.setUniform4f("color", 1.0f, 1.0f, 1.0f, 1.0f);
+	modelview = glm::mat4(1.0f);
+	texProgram.setUniformMatrix4f("modelview", modelview);
+	map->render();
+}
+
+void Scene::initShaders()
+{
+	Shader vShader, fShader;
+
+	vShader.initFromFile(VERTEX_SHADER, "shaders/texture.vert");
+	if(!vShader.isCompiled())
+	{
+		cout << "Vertex Shader Error" << endl;
+		cout << "" << vShader.log() << endl << endl;
+	}
+	fShader.initFromFile(FRAGMENT_SHADER, "shaders/texture.frag");
+	if(!fShader.isCompiled())
+	{
+		cout << "Fragment Shader Error" << endl;
+		cout << "" << fShader.log() << endl << endl;
+	}
+	texProgram.init();
+	texProgram.addShader(vShader);
+	texProgram.addShader(fShader);
+	texProgram.link();
+	if(!texProgram.isLinked())
+	{
+		cout << "Shader Linking Error" << endl;
+		cout << "" << texProgram.log() << endl << endl;
+	}
+	texProgram.bindFragmentOutput("outColor");
+	vShader.free();
+	fShader.free();
+}
+
+
+
