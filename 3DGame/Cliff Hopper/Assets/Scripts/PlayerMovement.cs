34148392ee42fbb1fb5e4af1d95eb0e18d8a9979
marcordonez <marc.ordonez@estudiantat.upc.edu>
2023-05-06 21:29:13 +0200

    Add 3D blocks and textures

--- 3DGame/Cliff Hopper/Assets/Scripts/PlayerMovement.cs
+++ 3DGame/Cliff Hopper/Assets/Scripts/PlayerMovement.cs
@@ -10,21 +10,27 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     bool bReleased;
     float angle;
-    MoveDirection currentDirection, moveDirection;
+    MoveDirection currentDirection;
     Vector3 initPos;
     Vector3 movement;
     Rigidbody rb;
-    public float jumpSpeed = 5000f;
-    bool canJump;
+    bool isCorner;
+
+    public Vector3 jump;
+    float jumpForce = 60.0f;
+
+    public bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
         // bRotating = false;
-        bReleased = true;
+        // bReleased = true;
         currentDirection = MoveDirection.RIGHT;
         movement = new Vector3 (0, 0, 1);
         rb = GetComponent<Rigidbody>();
+
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
     }
 
 
@@ -32,34 +38,36 @@ public class PlayerMovement : MonoBehaviour
     {
         if(other.gameObject.tag == "Corner")
         {
-            canJump = false;
+            isCorner = true;
         }
-        else canJump = true;
+        else isCorner = false;
+    }
+
+    void OnCollisionStay(){
+        isGrounded = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!bReleased && !(Input.GetKey(KeyCode.Space))) {
-            bReleased = true;
-        }
-        if (bReleased && (Input.GetKey(KeyCode.Space))) {
-            bReleased = false;
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)  {
             initPos = transform.position;
-            if (!canJump) {
+            if (isCorner) {
                 if (currentDirection == MoveDirection.RIGHT) {
                     currentDirection = MoveDirection.LEFT;
                     transform.Rotate(0.0f, 90.0f, 0.0f);
-                    moveDirection = MoveDirection.RIGHT;
                 }
                 else if (currentDirection == MoveDirection.LEFT) {
                     currentDirection = MoveDirection.RIGHT;
                     transform.Rotate(0.0f, -90.0f, 0.0f);
-                    moveDirection = MoveDirection.LEFT;
                 }
             }
             else {
-                rb.AddForce(0f, jumpSpeed * Time.deltaTime, 0f);
+                //rb.AddForce(0f, jumpSpeed * Time.deltaTime, 0f);
+                //rb.AddForce(0, jumpSpeed, 0, ForceMode.Impulse);
+                // rb.AddForce(Vector3.up * jumpSpeed,ForceMode.Impulse);
+                rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+                isGrounded = false;
             }
         }
         // move the player
