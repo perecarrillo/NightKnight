f1a8366e50e1c9fe7a37d6621f4046c96b17eca1
perecarrillo <pere.carrillo.barrera@estudiantat.upc.edu>
2023-05-08 21:46:49 +0200

    Add rotations & things

--- 3DGame/Cliff Hopper/Assets/Scripts/LevelCreator.cs
+++ 3DGame/Cliff Hopper/Assets/Scripts/LevelCreator.cs
@@ -7,9 +7,8 @@ enum TerrainType {Button, Plain, Stair, Spike};
 public class LevelCreator : MonoBehaviour
 {
     public GameObject buttonPrefab, plainPrefab, stairPrefab, spikePrefab;
-    int numTiles = 3;
-    bool gapBefore = true;
-    bool spikeBefore = true;
+    bool noGap = true;
+    bool noSpike = true;
     bool left = false;
     System.Random random = new System.Random();
 
@@ -25,24 +24,41 @@ public class LevelCreator : MonoBehaviour
             int size = random.Next(2, 10);
             if (left) ++pos.x;
             else ++pos.z;
+            noGap = true; // To not put a jump right after a turn
+            noSpike = true;
             for (uint j = 0; j < size; ++j) {
                 int rand = random.Next(100);
                 if (rand < 20) type = TerrainType.Stair;
-                else if (!gapBefore && rand < 30) {
+                else if (!noGap && rand < 30) {
                     if (left) ++pos.x;
                     else ++pos.z;
-                    gapBefore = true;
+                    noGap = true;
                     continue;
                 }
-                else if (rand < 40) type = TerrainType.Spike;
+                else if (!noSpike && rand < 40) {
+                    type = TerrainType.Spike;
+                    if (left) ++pos.x;
+                    else ++pos.z;
+                    noSpike = true;
+                    continue;
+                }
                 else type = TerrainType.Plain;
                 obj = (GameObject)Instantiate(GetPrefabTerrainType(type));
                 obj.transform.Translate(pos);
                 obj.transform.parent = transform;
-                if (left) ++pos.x;
+                if (left) {
+                    ++pos.x;
+                }
                 else ++pos.z;
-                if (type == TerrainType.Stair) --pos.y;
-                gapBefore = false;
+                if (type == TerrainType.Stair) {
+                    pos.y -= 0.5f;
+                    if (left) obj.transform.Rotate(new Vector3(0, 90, 0));
+                }
+                else {
+                    obj.transform.Rotate(new Vector3(0, random.Next(4) * 90,0));
+                }
+                noGap = false;
+                noSpike = false;
             }
             left = !left;
         }
