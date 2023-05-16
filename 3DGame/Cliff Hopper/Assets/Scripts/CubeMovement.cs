2e0b6c0fd5962fa53f557fb73af4d0927eee641a
perecarrillo <pere.carrillo.barrera@estudiantat.upc.edu>
2023-05-16 13:46:12 +0200

    Fix bugs

--- 3DGame/Cliff Hopper/Assets/Scripts/CubeMovement.cs
+++ 3DGame/Cliff Hopper/Assets/Scripts/CubeMovement.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -52,3 +52,4 @@ public class CubeMovement : MonoBehaviour
         _isMoving = false;
     }
 }
+*/
